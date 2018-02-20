e053d5c2f82172379ba5a772e5d1a7ec28081638
Andersen Pecorone <andersen.pecorone@atenta.eu>
2018-02-20 16:13:12 +0100

    Adding Models and Views

--- ListofPharmaceuticalProducts/models.py
+++ ListofPharmaceuticalProducts/models.py
@@ -9,12 +9,12 @@ from DjangoPython.core.models import AuditTrail
 class MedicalList(AuditTrail):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, null=False, blank=False)
-    composition = models.CharField(max_length=250)
-    reimbursible = models.CharField(max_length=100)
-    usage = models.CharField(max_length=250)
-    comments = models.CharField(max_length=250)
+    composition = models.CharField(max_length=250, null=True, blank=True)
+    reimbursible = models.CharField(max_length=100, null=True, blank=True)
+    usage = models.CharField(max_length=250, null=True, blank=True)
+    comments = models.CharField(max_length=250, null=True, blank=True)
     app_date = models.DateField(auto_now=True)
-    link = models.URLField(max_length=250)
+    link = models.URLField(max_length=250, null=True, blank=True)
 
     def __str__(self):
         return self.name
