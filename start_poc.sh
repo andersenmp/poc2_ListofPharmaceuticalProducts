e08e688e51f66ed6bce161ff5bcca1810638adb6
Andersen Pecorone <andersenmp@gmail.com>
2018-05-24 11:39:59 +0200

    Show IP for the VM

--- start_poc.sh
+++ start_poc.sh
@@ -1,3 +1,4 @@
-source ~/.bash_profile
+IP="$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')"
+echo "Server will start at http://$IP:8000"
 source venv/bin/activate
 python manage.py runserver 0.0.0.0:8000
